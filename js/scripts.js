97cfe4fa6fc6686ba1dee07bf9cd896e96535deb
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-14 14:26:40 -0700

    wrote the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,48 @@
-// function noVowel(str) {
-//   let oldStr = str.trim();
-//   let strArr = oldStr.split('');
-//   console.log(strArr);
-//   for (let x = 0; x < str.length; x++) {
-//     let char = str[x].toLowerCase();
-//     if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
-//       strArr[x] = '-';
-//     }
-//   }
-//   console.log(strArr.join(''));
+function BankAccount(name, initial_deposit) {
+  this.name = name;
+  this.initial_deposit = initial_deposit;
+}
+
+// function Dog(name, colors, age) {
+//   this.name = name;
+//   this.colors = colors;
+//   this.age = age;
 // }
-// const vowels = ['a','e','i','o','u'];
 
-function vowelFinder(str) {
-  let array1 = str.split();
-  for (let i = 0; i < array1.length; i++) {
-    let char = str[i];
-    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-function consonantFinder(str) {
-  let array1= str.split();
-  for (let i = 0; i < array1.length; i++) {
-    let char = str[i];
-    if (char !== 'a' && char !== "e" && char !== "i" && char !== "o" && char !== "u") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-function checkForQu(str) {
-  let array1= str.split();
-  for (let i = 0; i < array1.length; i++) {
-    let char1 = str[i]; 
-    let char2 = str[i+1];
-    if (char1 === 'q' && char2 === "u" ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-function addWay(str) {
-  let array1 = str + 'way';
-  console.log(array1);
-  }
-
-function addAy(str) {
-  let array1 = str + 'ay';
-  console.log(array1);
-  }
+// let falcor = new Dog("Falcor", ["black"], 4);
+// let nola = new Dog("Nola", ["white", "black"], 6);
+// let patsy = new Dog("Patsy", ["brown"], 7);
 
 
-// function consonants(str) {
-//   var countConsonants = 0;
 
-//   for (var i = 0; i <= str.length; i++) {
 
-//     if (str[i] !== "a" || str[i] !== "e" || str[i] !== "i" ||
-//       str[i] !== "o" || str[i] !== "u" || str[i] !== " ") {
-//       countConsonants += 1;
-//     }
-//   }
-//   return (countConsonants);
-// }
-// consonants("asdfghaaa");
+  
+
+// UI logic
+$(document).ready(function() {
+  $("form#register").submit(function(event) {
+    event.preventDefault();
+    const title = $("#name").val();
+    const time = parseInt($("#deposit").val());
+    const age = parseInt($("#question3").val());
+    let matrix = new Movie("Matrix", ["1:00pm", "3:00pm", "8:00pm"], true);
+    let madagascar = new Movie("Madagascar", ["1:00pm", "8:00pm"], false);
+    let incredibles = new Movie("The Incredibles", ["1:00pm", "3:00pm", "8:00pm"], true);
+    $("#total-due").html(Discount(time, age));
+
+  });
+});
+
+$(document).ready(function() {
+  $("form#depositwithdrawal").submit(function(event) {
+    event.preventDefault();
+    const title = parseInt($("#question1").val());
+    const time = parseInt($("#question2").val());
+    const age = parseInt($("#question3").val());
+    let matrix = new Movie("Matrix", ["1:00pm", "3:00pm", "8:00pm"], true);
+    let madagascar = new Movie("Madagascar", ["1:00pm", "8:00pm"], false);
+    let incredibles = new Movie("The Incredibles", ["1:00pm", "3:00pm", "8:00pm"], true);
+    $("#total-due").html(Discount(time, age));
+
+  });
+});
